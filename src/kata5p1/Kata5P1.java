93886ccd4b1f1c16f722be5c1cc044b41b8e0c37
vj_62 <vj_62@DESKTOP-ES4EHF5>
2021-12-30 14:06:02 +0000

    Segunda version Kata5P1

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,12 +1,30 @@
 package kata5p1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Kata5P1 {
+    
+    public static void createNewTable() {
+
+        String url = "jdbc:sqlite:EMAIL.db";
+        String sql = "CREATE TABLE IF NOT EXISTS EMAIL (\n"
+                    + " id integer PRIMARY KEY AUTOINCREMENT,\n"
+                    + " mail text NOT NULL);";
+        try (Connection conn = DriverManager.getConnection(url);
+        Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println("Tabla creada");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 
     public static void main(String[] args) {
-        SelectApp app = new SelectApp();
-        app.selectAll();
+        createNewTable();
 
     }
 
